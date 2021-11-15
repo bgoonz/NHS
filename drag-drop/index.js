52a7cd90cba5c11d348fe6a94151441dbd171255
Bryan C Guner <bryan.guner@gmail.com>
2021-11-15 09:13:10 +0000

    update

---
+++ drag-drop/index.js
@@ -0,0 +1,113 @@
+module.exports = dragDrop
+
+var flatten = require('flatten')
+var parallel = require('run-parallel')
+var throttle = require('lodash.throttle')
+
+function dragDrop (elem, ondrop) {
+  if (typeof elem === 'string') elem = window.document.querySelector(elem)
+
+  var onDragOver = makeOnDragOver(elem)
+  var onDrop = makeOnDrop(elem, ondrop)
+
+  elem.addEventListener('dragenter', stopEvent, false)
+  elem.addEventListener('dragover', onDragOver, false)
+  elem.addEventListener('drop', onDrop, false)
+
+  // Function to remove drag-drop listeners
+  return function remove () {
+    if (elem instanceof window.Element) elem.classList.remove('drag')
+    elem.removeEventListener('dragenter', stopEvent, false)
+    elem.removeEventListener('dragover', onDragOver, false)
+    elem.removeEventListener('drop', onDrop, false)
+  }
+}
+
+function stopEvent (e) {
+  e.stopPropagation()
+  e.preventDefault()
+  return false
+}
+
+function makeOnDragOver (elem) {
+  var fn = throttle(function () {
+    if (elem instanceof window.Element) elem.classList.add('drag')
+
+    if (elem.timeout) clearTimeout(elem.timeout)
+    elem.timeout = setTimeout(function () {
+      if (elem instanceof window.Element) elem.classList.remove('drag')
+    }, 150)
+  }, 100, {trailing: false})
+
+  return function (e) {
+    e.stopPropagation()
+    e.preventDefault()
+    e.dataTransfer.dropEffect = 'copy'
+    fn()
+  }
+}
+
+function makeOnDrop (elem, ondrop) {
+  return function (e) {
+    e.stopPropagation()
+    e.preventDefault()
+    if (elem instanceof window.Element) elem.classList.remove('drag')
+    var pos = { x: e.clientX, y: e.clientY }
+    if (e.dataTransfer.items) {
+      // Handle directories in Chrome using the proprietary FileSystem API
+      parallel(toArray(e.dataTransfer.items).map(function (item) {
+        return function (cb) {
+          processEntry(item.webkitGetAsEntry(), cb)
+        }
+      }), function (err, results) {
+        if (err) return // there will never be an err here
+        ondrop(flatten(results), pos)
+      })
+    } else {
+      var files = toArray(e.dataTransfer.files)
+      files.forEach(function (file) {
+        file.fullPath = '/' + file.name
+      })
+      ondrop(files, pos)
+    }
+
+    return false
+  }
+}
+
+function processEntry (entry, cb) {
+  var entries = []
+
+  if (entry.isFile) {
+    entry.file(function (file) {
+      file.fullPath = entry.fullPath  // preserve pathing for consumer
+      cb(null, file)
+    })
+  } else if (entry.isDirectory) {
+    var reader = entry.createReader()
+    readEntries()
+  }
+
+  function readEntries () {
+    reader.readEntries(function (entries_) {
+      if (entries_.length > 0) {
+        entries = entries.concat(toArray(entries_))
+        readEntries() // continue reading entries until `readEntries` returns no more
+      } else {
+        doneEntries()
+      }
+    })
+  }
+
+  function doneEntries () {
+    parallel(entries.map(function (entry) {
+      return function (cb) {
+        processEntry(entry, cb)
+      }
+    }), cb)
+  }
+}
+
+function toArray (list) {
+  return Array.prototype.slice.call(list || [], 0)
+}
